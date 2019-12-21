9f543ae338486dbb566d92b324f63a4eb0012d3b
dthirumalaibe <35888599+dthirumalaibe@users.noreply.github.com>
2019-12-21 09:47:54 -0800

    Update write_1.py

--- write_1.py
+++ write_1.py
@@ -1,3 +1,4 @@
 with open("/tmp/test_thiru.txt","w") as f:
     f.write("written from Jenkins via GIT")
     f.write("written from Jenkins via GIT222")
+    f.write("written from Jenkins via GIT333")
